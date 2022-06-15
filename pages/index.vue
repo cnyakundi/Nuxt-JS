e33fe01ad4f51ada346e12ae191b5852433ba311
Cyprian Nyakundi <cn@Cyprians-MacBook-Pro.local>
2022-06-15 10:45:48 +0300

    Initial

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="container">
+ <Hero />
+ <LargeCardDisplay 
+ v-for="cardInfo in largeCardInfo"
+ :key="cardInfo.id"
+ :cardsSection="cardInfo"
+ />
+ </div>
+</template>
+
+<script>
+import {largeCardSections} from "@/assets/data.js"
+import LargeCardDisplayVue from "../components/LargeCardDisplay.vue";
+export default {
+  data() {
+    return {
+      largeCardInfo : largeCardSections
+      
+    }
+  },
+    
+}
+</script>
