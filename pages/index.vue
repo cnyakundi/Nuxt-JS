a319ea909bae35bcbf2245d168da6afd8ee1816d
Cyprian Nyakundi <cn@Cyprians-MacBook-Pro.local>
2022-06-15 19:47:35 +0300

    Initial

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,32 @@
 <template>
  <div class="container">
+ <Nav />
  <Hero />
  <LargeCardDisplay 
  v-for="cardInfo in largeCardInfo"
  :key="cardInfo.id"
  :cardsSection="cardInfo"
  />
+ <SmallCardDisplay
+ v-for="cardInfo in smallCardInfo"
+ :key="cardInfo.id"
+ :cardsSection="cardInfo"
+ 
+  />
  </div>
 </template>
 
 <script>
-import {largeCardSections} from "@/assets/data.js"
+import {largeCardSections, smallCardSections} from "@/assets/data.js"
 import LargeCardDisplayVue from "../components/LargeCardDisplay.vue";
+import SmallCardDisplayVue from "../components/SmallCardDisplay.vue";
 export default {
-  data() {
-    return {
-      largeCardInfo : largeCardSections
-      
-    }
-  },
-    
+    data() {
+        return {
+            largeCardInfo: largeCardSections,
+            smallCardInfo:smallCardSections
+        };
+    },
+    components: { SmallCardDisplayVue }
 }
 </script>
---
+++ pages/products/index.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+<Nav />
+<div class="container">
+   <LargeCardDisplay 
+   
+   v-for="cardInfo in largeCardInfo.slice(0,1)"
+   :key="cardInfo.id"
+   :cardsSection="cardInfo"
+   />
+
+
+   <LargeCardDisplay 
+   
+   v-for="cardInfo in smallCardInfo.slice(0,1)"
+   :key="cardInfo.id"
+   :cardsSection="cardInfo"
+   />
+</div>
+  </div>
+</template>
+
+<script>
+import {largeCardSections, smallCardSections} from  '@/assets/data.js'
+export default {
+
+
+    data() {
+        return {
+            largeCardInfo: largeCardSections,
+            smallCardInfo:smallCardSections
+            
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
